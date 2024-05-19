48d8da5f4cf5fe48b788f0e3bd26a404264e22c3
chesedo <pieter@chesedo.me>
2024-05-19 22:03:46 +0100

    Minimal working version

---
+++ README.md
@@ -0,0 +1,64 @@
+# Sandy Image Library
+
+The Sandy Image Library is a JavaScript library that creates a mesmerizing sand animation effect by drawing an `<img>` tag in grayscale using sand grains. The sand grains are dynamically repelled from the mouse or touch input, creating an interactive and visually appealing experience.
+
+## Features
+
+- Converts an image into a grayscale sand animation
+- Sand grains are repelled from the mouse or touch input
+- Customizable options for grain size, animation steps, and more
+- Smooth and efficient animation using a worker thread
+- Adjustable wind effect for dynamic animations
+
+## Installation
+
+You can install the Sandy Image Library via npm:
+
+```bash
+npm install sandy-image
+```
+
+Alternatively, you can include the library files directly in your project:
+
+```html
+<script src="path/to/sandy.js"></script>
+<!-- Make sure sandy-worker.js is in the same directory -->
+```
+
+## Usage
+
+1. Create an instance of the SandyImage class, passing the selector of the target image and any desired options:
+
+    ```javascript
+    const sandyImage = new SandyImage('#myImage', {
+    size: 6,
+    steps: 5,
+    // Other options...
+    });
+    ```
+
+2. The library will automatically replace the target image with a canvas and start the sand animation.
+
+3. Customize the animation by providing options during initialization. The available options include:
+    - `size`: The pixel size of each sand grain (default: 6). Bigger sizes result in a faster animation but reduces the detail in the final image.
+    - `steps`: The number of grayscale steps the image should be converted into (default: 5). Smaller steps result in a faster animation, but values lower than 5 tend to have clear grayscale steps.
+    - `stepDepth`: The artificial "depth" added to each grayscale step (default: 3). This makes it possible to use bigger sizes and smaller steps by adding an artificial detail to the final image. Values between 2 and 5 tend to work well.
+    - `targetFPS`: The target frames per second for the animation (default: 30).
+    - `dragCoefficient`: The drag coefficient for sand movement (default: 0.2). Values bigger than 0.5 will cause the grains to jiggle.
+    - `repelRadius`: The radius within which sand particles are repelled by the mouse or touch input (default: 20).
+    - `createGrainFn`: A custom function for creating sand grains. It takes an object with properties `canvasWidth`, `canvasHeight`, `size`, and `index` as input and should return an object with `x`, `y`, `vx`, and `vy` properties for the initial grain's position and velocity.
+    - `breezeFn`: A custom function for applying a breeze effect to the animation. It takes an object with properties `frame`, `globalVx`, and `globalVy` as input and should return an object with updated `globalVx` and `globalVy` values.
+    - `debug`: Enable debug mode for performance logging (default: false).
+
+4. The sand animation will automatically respond to mouse or touch input, repelling the sand particles within the specified repelRadius.
+
+## Performance Considerations
+
+Keep the grain count less than 20,000 for optimal performance. This can be achieved by editing the image ahead of time to have as much white space as possible and clear constrast between objects in the image.
+Use the debug option during development to monitor the animation's performance and frames per second (FPS).
+
+## Contributing
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request on the GitHub repository.
+
+## License
+This library is released under the MIT License.
---
+++ sandy.js
@@ -0,0 +1,260 @@
+function SandyImage(imgSelector, options) {
+    this.imgSelector = imgSelector;
+    options = {
+        size: 6,
+        steps: 5,
+        stepDepth: 3,
+        targetFPS: 30,
+        dragCoefficient: 0.2,
+        repelRadius: 20,
+        createGrainFn: ({ canvasWidth, canvasHeight, size, index }) => {
+            const x = Math.random() * (canvasWidth - size) + size / 2;
+            const y = Math.random() * (canvasHeight - size) + size / 2;
+            const vx = (Math.random() - 0.5) * size * 100;
+            const vy = (Math.random() - 0.5) * size * 100;
+
+            return { x, y, vx, vy };
+        },
+        breezeFn: ({ frame, globalVx, globalVy }) => {
+            if (frame % 30 === 0) {
+                globalVx += Math.random() * 1 - 0.5;
+                globalVy += Math.random() * 1 - 0.5;
+
+                globalVx = Math.tanh(globalVx);
+                globalVy = Math.tanh(globalVy);
+            }
+
+            return { globalVx, globalVy }
+        },
+        debug: false,
+        ...options
+    };
+    this.size = options.size;
+    this.steps = options.steps;
+    this.stepDepth = options.stepDepth;
+    this.targetFPS = options.targetFPS;
+    this.targetFrameTime = 1000 / this.targetFPS;
+    this.dragCoefficient = options.dragCoefficient;
+    this.repelRadius = options.repelRadius;
+    this.mouseX = -this.repelRadius;
+    this.mouseY = -this.repelRadius;
+    this.elevationData = null;
+    this.grainCount = 0;
+    this.createGrainFn = options.createGrainFn;
+    this.breezeFn = options.breezeFn;
+    this.globalVx = 0;
+    this.globalVy = 0;
+    this.frame = 0;
+    this.grains = null;
+    this.updating = false;
+
+    this.debug = options.debug;
+
+    this.init();
+}
+
+SandyImage.prototype.init = function () {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+
+    // Replace the image with the grains canvas
+    this.img = document.querySelector(this.imgSelector);
+    this.img.parentNode.insertBefore(this.canvas, this.img.nextSibling);
+    this.img.style.display = 'none';
+    this.img.crossOrigin = 'anonymous';
+
+    // Used to rebel the grains from the mouse
+    document.addEventListener('mousemove', this.handleInput.bind(this));
+    document.addEventListener('touchmove', this.handleInput.bind(this));
+    document.addEventListener('touchstart', this.handleInput.bind(this));
+
+    if (this.img.complete) {
+        this.loadElevationImage();
+    } else {
+        this.img.addEventListener('load', this.loadElevationImage.bind(this));
+    }
+};
+
+SandyImage.prototype.handleInput = function (event) {
+    event.preventDefault();
+
+    let clientX, clientY;
+    if (event.type === 'touchmove' || event.type === 'touchstart') {
+        clientX = event.touches[0].clientX;
+        clientY = event.touches[0].clientY;
+    } else {
+        clientX = event.clientX;
+        clientY = event.clientY;
+    }
+
+    this.mouseX = clientX - this.canvas.offsetLeft + window.scrollX;
+    this.mouseY = clientY - this.canvas.offsetTop + window.scrollY;
+};
+
+SandyImage.prototype.loadElevationImage = function () {
+    this.canvas.width = this.img.width;
+    this.canvas.height = this.img.height;
+    this.ctx.drawImage(this.img, 0, 0);
+
+    this.buildElevationData();
+    this.createGrains();
+    this.startWorker();
+    this.startAnimation();
+};
+
+// We need to read the image data to use the color (grayscale) to form an elevation array
+// which will be used to determine how many grains can be placed at certain pixels
+SandyImage.prototype.buildElevationData = function () {
+    this.elevationData = new Uint8Array(this.canvas.height * this.canvas.width);
+
+    const imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    const data = imageData.data;
+
+    // We will also want to normalize the elevation data
+    let minElevation = Infinity;
+    let maxElevation = -Infinity;
+
+    let index = 0;
+    for (let y = 0; y < this.canvas.height; y++) {
+        for (let x = 0; x < this.canvas.width; x++) {
+            const pixelIndex = (y * this.canvas.width + x) * 4;
+            const red = data[pixelIndex];
+            const green = data[pixelIndex + 1];
+            const blue = data[pixelIndex + 2];
+
+            let elevation = (red + green + blue) / 3;
+
+            // Dark colors are hills; light colors are approaching water
+            elevation = 255 - elevation;
+
+            minElevation = Math.min(minElevation, elevation);
+            maxElevation = Math.max(maxElevation, elevation);
+
+            this.elevationData[index] = elevation;
+            index++;
+        }
+    }
+
+    // Normalize the elevation data
+    let total = 0;
+    for (let i = 0; i < this.elevationData.length; i++) {
+        const norm = (this.elevationData[i] - minElevation) / (maxElevation - minElevation);
+        const scaled = Math.floor(norm * this.steps * this.stepDepth);
+
+        this.elevationData[i] = scaled;
+        total += scaled / this.stepDepth;
+    }
+
+    let squareArea = this.size * this.size;
+    this.grainCount = Math.floor(total / squareArea);
+
+    if (this.debug) {
+        console.log('total', total, 'grainCount', this.grainCount);
+    }
+};
+
+SandyImage.prototype.createGrains = function () {
+    this.grains = new Float32Array(this.grainCount * 4);
+
+    for (let i = 0; i < this.grainCount; i++) {
+        const { x, y, vx, vy } = this.createGrainFn({
+            canvasWidth: this.canvas.width,
+            canvasHeight: this.canvas.height,
+            size: this.size,
+            index: i
+        });
+
+        this.grains[i * 4] = x;
+        this.grains[i * 4 + 1] = y;
+        this.grains[i * 4 + 2] = vx;
+        this.grains[i * 4 + 3] = vy;
+    }
+};
+
+SandyImage.prototype.startWorker = function () {
+    this.worker = new Worker('sandy-worker.js');
+
+    // Send the initial data to the worker
+    this.worker.postMessage({
+        elevationData: this.elevationData,
+        grains: this.grains,
+        size: this.size,
+        width: this.canvas.width,
+        height: this.canvas.height,
+        steps: this.steps,
+        repelRadius: this.repelRadius,
+        dragCoefficient: this.dragCoefficient,
+        debug: this.debug,
+    });
+
+    let totalElapsedTime = 0;
+
+    // Update the canvas when there are new grain positions
+    this.worker.onmessage = function (event) {
+        const startTime = performance.now();
+
+        this.updating = false;
+        const updatedGrains = event.data.grains;
+
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.beginPath();
+
+        this.ctx.fillStyle = `rgba(0, 0, 0, ${1.0 / this.steps / this.stepDepth})`;
+
+        for (let i = 0; i < updatedGrains.length; i += 4) {
+            const x = updatedGrains[i];
+            const y = updatedGrains[i + 1];
+            this.ctx.fillRect(x, y, this.size, this.size);
+        }
+
+        this.ctx.fill();
+
+        if (this.debug) {
+            const endTime = performance.now();
+            totalElapsedTime += endTime - startTime;
+
+            if (this.frame % 50 === 0) {
+                const averageFPS = 1000 / (totalElapsedTime / this.frame);
+                console.log(`[${this.frame}]`, 'drawing averageFPS', averageFPS);
+            }
+        }
+    }.bind(this);
+};
+
+SandyImage.prototype.startAnimation = function () {
+    let lastFrameTime = performance.now();
+
+    const animationLoop = function () {
+        const currentTime = performance.now();
+        const elapsedTime = currentTime - lastFrameTime;
+
+        // Don't go over the target FPS
+        if (elapsedTime >= this.targetFrameTime && !this.updating) {
+            this.updating = true;
+
+            const { globalVx, globalVy } = this.breezeFn({
+                frame: this.frame,
+                globalVx: this.globalVx,
+                globalVy: this.globalVy,
+            });
+
+            this.globalVx = globalVx;
+            this.globalVy = globalVy;
+
+            this.worker.postMessage({
+                action: 'update',
+                mouseX: this.mouseX,
+                mouseY: this.mouseY,
+                globalVx,
+                globalVy,
+            });
+            lastFrameTime = currentTime;
+            this.frame++;
+        }
+
+        requestAnimationFrame(animationLoop);
+    }.bind(this);
+
+    animationLoop();
+};
